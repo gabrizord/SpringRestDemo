b42f243dd7a89be19ecae6e0870511ce66f5afcb
Gabriel <lolqueloko@gmail.com>
2024-05-27 21:49:46 -0300

    First Commit

---
+++ src/test/java/br/com/gabrizord/bmbackend/BmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrizord.bmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
