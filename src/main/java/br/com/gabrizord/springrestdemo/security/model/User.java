a6463637f7035fcd4d1dad9f65f64257b79c66dc
Gabriel <lolqueloko@gmail.com>
2024-07-14 13:06:26 -0300

    feat: Add security dependencies and user model implementation
    - Added `spring-boot-starter-security` and `spring-security-test` dependencies to `pom.xml`.
    - Created `User` entity implementing `UserDetails` with fields for id, username, password, and role.
    - Added `UserRole` enum to represent user roles.

---
+++ src/main/java/br/com/gabrizord/springrestdemo/security/model/User.java
@@ -0,0 +1,57 @@
+package br.com.gabrizord.springrestdemo.security.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/br/com/gabrizord/springrestdemo/security/model/UserRole.java
@@ -0,0 +1,8 @@
+package br.com.gabrizord.springrestdemo.security.model;
+
+public enum UserRole {
+    ADMIN,
+    USER
+
+
+}
